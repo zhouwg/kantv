3de657071d83c5bc0e75aa3c18e6e73abb86eec6
zhou.weiguo <zhouwg2000@gmail.com>
2024-03-01 23:26:07 +0800

    add empty prebuild_download.sh for further purpose

---
+++ build/prebuild_download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Copyright (c) Project KanTV. 2021-2023. All rights reserved.
+
+# Copyright (c) 2024- KanTV Authors. All Rights Reserved.
+
+
+# Description: tools & utilitis for build project
+
+
+
+
