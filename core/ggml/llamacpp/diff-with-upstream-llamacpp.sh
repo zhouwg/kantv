0ea54bf9059454fb8b486dcb3d61133f8f5c733a
zhouwg <6889919+zhouwg@users.noreply.github.com>
2024-04-09 11:53:35 +0800

    project: sync with upstream llama.cpp/ggml (#138)

--- core/ggml/llamacpp/diff-with-upstream-llamacpp.sh
+++ core/ggml/llamacpp/diff-with-upstream-llamacpp.sh
@@ -32,7 +32,7 @@ echo -e  "upstream llamacpp path: ${UPSTREAM_LLAMACPP_PATH}\n"
 echo -e  "local    llamacpp path: ${LOCAL_LLAMACPP_PATH}\n"
 
 #the following method borrow from bench-all.sh in GGML's project whisper.cpp
-LLAMACPP_SRCS=(ggml-alloc.c ggml-alloc.h ggml-backend.c ggml-backend.h ggml.c ggml.h ggml-quants.c ggml-quants.h llama.cpp llama.h unicode.h unicode.cpp unicode-data.h unicode-data.cpp ggml-common.h)
+LLAMACPP_SRCS=(ggml-alloc.c ggml-alloc.h ggml-backend.c ggml-backend.h ggml.c ggml.h ggml-quants.c ggml-quants.h llama.cpp llama.h unicode.h unicode.cpp unicode-data.h unicode-data.cpp ggml-common.h examples/main/main.cpp)
 for file in "${LLAMACPP_SRCS[@]}"; do
     echo "diff $file     ${UPSTREAM_LLAMACPP_PATH}/$file"
     diff ${LOCAL_LLAMACPP_PATH}/$file     ${UPSTREAM_LLAMACPP_PATH}/$file
