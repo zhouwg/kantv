6900d7e944cd6d9302f694f27ddef258e8c82a60
zhou.weiguo <zhouwg2000@gmail.com>
2024-04-10 11:27:48 +0800

    ggml-jni: fix misunderstanding from "compute graph" to "computation graph"

--- cdeosplayer/cdeosplayer-lib/src/main/java/org/ggml/ggmljava.java
+++ cdeosplayer/cdeosplayer-lib/src/main/java/org/ggml/ggmljava.java
@@ -121,7 +121,7 @@ public class ggmljava {
      * @param nBenchType    0: asr(transcription) 1: memcpy 2: mulmat  3: full/whisper_encode 4: matrix  5: LLAMA  6: stable diffusion 7: QNN sample 8: QNN saver 9: QNN matrix 10: QNN GGML 11: QNN complex
      * @param nThreadCounts 1 - 8
      * @param nBackendType  0: CPU  1: GPU  2: DSP 3: ggml("fake" QNN backend, just for compare performance)
-     * @param nOpType       type of matrix manipulate / GGML OP / type of various complex/complicated compute graph
+     * @param nOpType       type of matrix manipulate / GGML OP / type of various complex/complicated computation graph
      * @return
      */
     public static native String ggml_bench(String modelPath, String audioPath, int nBenchType, int nThreadCounts, int nBackendType, int nOpType);
