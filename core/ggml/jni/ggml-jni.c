36a20b68c0792f9cce886b97db8b803ff4f6a2d8
zhou.weiguo <zhouwg2000@gmail.com>
2024-05-21 14:38:36 +0800

    ncnn-jni: add RESNET inference using NCNN

--- core/ggml/jni/ggml-jni.c
+++ core/ggml/jni/ggml-jni.c
@@ -12,9 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * The above statement and notice must be included in corresponding files
- * in derived project
  */
 #include <jni.h>
 
