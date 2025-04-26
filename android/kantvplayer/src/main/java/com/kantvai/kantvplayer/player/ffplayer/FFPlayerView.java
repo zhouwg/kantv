zhouwg/kantv

commit e1d601945b6faef51f447c7c7c2ccab0bd0a85f0

ggml-hexagon: fix issue #269: HWACCEL_CDSP cannot works as expected in a standard Android APP (#281)
* build: fix CI

* ggml-hexagon:fix issue #269:cannot works fine as expected in a standard Android APP

* ggml-hexagon: fix issue with LLM inference in a standard Android APP

* ui: refine UI code

* doc:update toplevel README.md

* doc: refine toplevel README.md

android/kantvplayer/src/main/java/com/kantvai/kantvplayer/player/ffplayer/FFPlayerView.java
@@ -1575,9 +1575,9 @@ public class FFPlayerView extends FrameLayout implements PlayerViewListener {
 
         if (KANTVUtils.getASRSubsystemInit()) {
             if ((KANTVUtils.ASR_MODE_NORMAL == mSettings.getASRMode()) || (KANTVUtils.ASR_MODE_TRANSCRIPTION_RECORD == mSettings.getASRMode())) {
-                ggmljava.asr_reset(KANTVUtils.getDataPath() + "/models/" + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_NORMAL, KANTVUtils.HEXAGON_BACKEND_GGML);
+                ggmljava.asr_reset(KANTVUtils.getDataPath() + "/models/" + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_NORMAL, ggmljava.HEXAGON_BACKEND_GGML);
             } else {
-                ggmljava.asr_reset(KANTVUtils.getDataPath() + "/models/" + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_PRESURETEST, KANTVUtils.HEXAGON_BACKEND_GGML);
+                ggmljava.asr_reset(KANTVUtils.getDataPath() + "/models/" + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_PRESURETEST, ggmljava.HEXAGON_BACKEND_GGML);
             }
             ggmljava.asr_start();
         } else {

core/ggml/jni/ggml-jni-impl.cpp
@@ -15,8 +15,6 @@
 
  * https://github.com/zhouwg/kantv/blob/kantv-poc-with-whispercpp/external/whispercpp/jni/whispercpp-jni.c
 
- * https://github.com/zhouwg/kantv/blob/kantv-poc-with-whispercpp/cdeosplayer/cdeosplayer-lib/src/main/java/org/ggml/whispercpp/whispercpp.java
- *
  *
  * in short, it's a very concise implementation and the method here is never seen in any other similar
  *
@@ -568,7 +566,7 @@ static int whisper_bench_full() {
  * @param model_path       /sdcard/kantv/ggml-xxxxx.bin
  * @param audio_path       /sdcard/kantv/jfk.wav
  * @param num_threads      1 - 8
- * @param n_backend_type   0: hexagon npu, 1: ggml
+ * @param n_backend_type   0: HEXAGON_BACKEND_QNNCPU 1: HEXAGON_BACKEND_QNNGPU 2: HEXAGON_BACKEND_QNNNPU, 3: HEXAGON_BACKEND_CDSP 4: ggml
  *
  * @return asr result which generated by whispercpp's inference
  */
@@ -634,7 +632,7 @@ static const char * ggml_jni_transcribe_from_file(const char * sz_model_path, co
         whisper_free(p_asr_ctx->p_context);
         struct whisper_context_params wcp = whisper_context_default_params();
         LOGGD("backend %d", n_backend_type);
-        wcp.gpu_device  = n_backend_type + 2;
+        wcp.gpu_device  = n_backend_type;
         wcp.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
         if (n_backend_type != 1) {
@@ -800,11 +798,11 @@ int ggml_jni_get_cpu_core_counts() {
   * @param sz_user_data    ASR: /sdcard/kantv/jfk.wav / LLM: user input / TEXT2IMAGE: user input / MNIST: image path / TTS: user input
   * @param n_bench_type    0: memcpy 1: mulmat 2: QNN GGML OP(QNN UT) 3: QNN UT automation 4: ASR(whisper.cpp) 5: LLM(llama.cpp)
   * @param n_threads       1 - 8
-  * @param n_backend_type  0: CPU  1: GPU  2: NPU 3: ggml("fake" QNN backend, just for compare performance)
-  * @param n_op_type       type of GGML OP
+  * @param n_backend_type  0: HEXAGON_BACKEND_QNNCPU 1: HEXAGON_BACKEND_QNNGPU 2: HEXAGON_BACKEND_QNNNPU, 3: HEXAGON_BACKEND_CDSP 4: ggml
+  * @param n_accel_type    0: HWACCEL_QNN 1: HWACCEL_QNN_SINGLEGRAPH 2: HWACCEL_CDSP
   * @return
 */
-void ggml_jni_bench(const char * sz_model_path, const char * sz_user_data, int n_bench_type, int n_threads, int n_backend_type, int n_op_type) {
+void ggml_jni_bench(const char * sz_model_path, const char * sz_user_data, int n_bench_type, int n_threads, int n_backend_type, int n_accel_type) {
     int result = 0;
 
     if (NULL == p_asr_ctx) {
@@ -826,11 +824,12 @@ void ggml_jni_bench(const char * sz_model_path, const char * sz_user_data, int n
     LOGGD("user data: %s\n", sz_user_data);
     LOGGD("bench type:%d\n", n_bench_type);
     LOGGD("backend type:%d\n", n_backend_type);
-    LOGGD("op type:%d\n", n_op_type);
+    LOGGD("accel type:%d\n", n_accel_type);
+
 #ifdef GGML_USE_HEXAGON
     if (HEXAGON_BACKEND_GGML == n_backend_type) {
         p_asr_ctx->b_use_gpu = false;
-        p_asr_ctx->gpu_device = HEXAGON_BACKEND_GGML;// HEXAGON_BACKEND_GGML is a fake QNN backend, just used for compare performance between QNN backend and original ggml
+        p_asr_ctx->gpu_device = HEXAGON_BACKEND_GGML;
     } else {
         p_asr_ctx->b_use_gpu = true;
         p_asr_ctx->gpu_device = n_backend_type;
@@ -858,23 +857,12 @@ void ggml_jni_bench(const char * sz_model_path, const char * sz_user_data, int n
             break;
 
         case GGML_BENCHMARK_LLM:
-            llama_inference_ng(sz_model_path, sz_user_data, n_bench_type, n_threads, n_backend_type);
+            llama_inference_ng(sz_model_path, sz_user_data, n_bench_type, n_threads, n_backend_type, n_accel_type);
             break;
 
         default:
             break;
     }
-}
-
-
-//"m" for "multimodal"
-void ggml_jni_bench_m(const char * sz_model_path, const char * sz_img_path, const char * sz_user_data, int n_bench_type, int n_num_threads, int n_backend_type) {
-    LOGGD("model path:%s\n", sz_model_path);
-    LOGGD("img path:%s\n", sz_img_path);
-    LOGGD("user data: %s\n", sz_user_data);
-    LOGGD("bench type:%d\n", n_bench_type);
-    LOGGD("backend type:%d\n", n_backend_type);
-    GGML_JNI_NOTIFY("not supported");
 }
 
 // =================================================================================================
@@ -1400,7 +1388,7 @@ int whisper_asr_init(const char * sz_model_path, int n_threads, int n_asrmode, i
      struct whisper_context_params c_params = whisper_context_default_params();
 
      //FIXME:hardcoded binary ggml backend path
-     char * backend_lib_path = "/data/data/com.cdeos.kantv/qnnlib";
+     char * backend_lib_path = "/data/data/com.kantvai.kantvplayer/";
      ggml_backend_load_all_from_path(backend_lib_path);
 
      if ((NULL == sz_model_path) || (n_asrmode > 3)) {
@@ -1432,7 +1420,7 @@ int whisper_asr_init(const char * sz_model_path, int n_threads, int n_asrmode, i
      }
      memset(p_asr_ctx->sz_model_path, 0, MAX_PATH_LEN);
      memcpy(p_asr_ctx->sz_model_path, sz_model_path, strlen(sz_model_path));
-     p_asr_ctx->n_backend = n_backend;//added on 04-17-2024, for PoC:Add Qualcomm mobile SoC native backend for GGML, https://github.com/zhouwg/kantv/issues/121
+     p_asr_ctx->n_backend = n_backend;
 
      result  = pthread_mutex_init(&p_asr_ctx->mutex, NULL);
      if (result != 0) {
@@ -1466,7 +1454,7 @@ int whisper_asr_init(const char * sz_model_path, int n_threads, int n_asrmode, i
      p_asr_ctx->n_threads  = n_threads;
 
      LOGGD("calling whisper_init_from_file");
-     c_params.gpu_device  = n_backend + 2;
+     c_params.gpu_device  = n_backend;
      c_params.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
      if (n_backend != 1) {
@@ -1508,7 +1496,7 @@ int whisper_asr_init(const char * sz_model_path, int n_threads, int n_asrmode, i
      params.debug_mode              = false;
      params.audio_ctx               = 0;
 
-     params.suppress_blank              = true;
+     params.suppress_blank          = true;
 
      //ref: https://github.com/ggerganov/whisper.cpp/issues/1507
      //reduce the maximum context size (--max-context). By default it is 224. Setting it to 64 or 32 can reduce the repetitions significantly. Setting it to 0 will most likely eliminate all repetitions, but the transcription quality can be affected because it will be losing the context from the previous transcript
@@ -1651,7 +1639,7 @@ int whisper_asr_reset(const char * sz_model_path, int n_threads, int n_asrmode, 
             whisper_free(p_asr_ctx->p_context);
         }
         struct whisper_context_params wcp = whisper_context_default_params();
-        wcp.gpu_device  = n_backend + 2;
+        wcp.gpu_device  = n_backend;
         wcp.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
         if (n_backend != 1) {
@@ -1669,7 +1657,7 @@ int whisper_asr_reset(const char * sz_model_path, int n_threads, int n_asrmode, 
             whisper_free(p_asr_ctx->p_context);
         }
         struct whisper_context_params wcp = whisper_context_default_params();
-        wcp.gpu_device  = n_backend + 2;
+        wcp.gpu_device  = n_backend;
         wcp.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
         if (n_backend != 1) {

.github/workflows/build.yml
@@ -9,7 +9,7 @@ on:
     branches:
       - master
      # 添加忽略路径，避免文档更新等触发构建
-    paths-ignore:     
+    paths-ignore:
       - '**.md'
       - 'docs/**'
       - '.gitignore'
@@ -28,12 +28,12 @@ jobs:
         with:
           submodules: 'recursive'
           fetch-depth: '0'
-        
+
       - name: build android release
         run: |
-          source build/envsetup.sh  
-          ./build/prebuild-download.sh  
-          lunch 1  
+          source build/envsetup.sh
+          ./build/prebuild-download.sh
+          lunch 1
           ./build-all.sh android
 
       - name: upload arm64-v8a release
@@ -41,16 +41,16 @@ jobs:
         if: ${{  success() }}
         with:
           name: arm64-v8a release apk
-          compression-level: 0 
+          compression-level: 0
           retention-days: 3
-          path: ${{ github.workspace }}/cdeosplayer/kantv/build/outputs/apk/all64/release/kantv-release-*-signed.apk
-          
+          path: ${{ github.workspace }}/android/kantvplayer/build/outputs/apk/all64/release/kantv-release-*-signed.apk
+
       - name: upload arm64-v8a release to release
         uses: svenstaro/upload-release-action@v2
         if: github.event_name == 'release'
         with:
           repo_token: ${{ secrets.GITHUB_TOKEN }}
-          file: ${{ github.workspace }}/cdeosplayer/kantv/build/outputs/apk/all64/release/kantv-release-*-signed.apk
+          file: ${{ github.workspace }}/android/kantvplayer/build/outputs/apk/all64/release/kantv-release-*-signed.apk
           tag: ${{ github.ref }}
           file_glob: true
 

.gitignore
@@ -79,3 +79,4 @@ crash.log
 kantv.jks
 core/ggml/llamacpp/tests/ggml-qnn-ut-4d.cpp
 CMakeLists.txt
+libggmlop-skel*.so

android/kantvplayer-lib/src/main/java/kantvai/ai/ggmljava.java
@@ -6,106 +6,20 @@ package kantvai.ai;
 public class ggmljava {
     private static final String TAG = ggmljava.class.getName();
 
-    //keep sync between here and ggml.h
-    // available GGML tensor operations:
-   public enum ggml_op {
-         GGML_OP_NONE,
-         GGML_OP_DUP,
-         GGML_OP_ADD,
-         GGML_OP_ADD1,
-         GGML_OP_ACC,
-         GGML_OP_SUB,
-         GGML_OP_MUL,
-         GGML_OP_DIV,
-         GGML_OP_SQR,
-         GGML_OP_SQRT,
-         GGML_OP_LOG,
-         GGML_OP_SIN,
-         GGML_OP_COS,
-         GGML_OP_SUM,
-         GGML_OP_SUM_ROWS,
-         GGML_OP_MEAN,
-         GGML_OP_ARGMAX,
-         GGML_OP_COUNT_EQUAL,
-         GGML_OP_REPEAT,
-         GGML_OP_REPEAT_BACK,
-         GGML_OP_CONCAT,
-         GGML_OP_SILU_BACK,
-         GGML_OP_NORM, // normalize
-         GGML_OP_RMS_NORM,
-         GGML_OP_RMS_NORM_BACK,
-         GGML_OP_GROUP_NORM,
+    //keep sync with ggml-hexagon.cpp
+    public static final int HEXAGON_BACKEND_QNNCPU = 0;
+    public static final int HEXAGON_BACKEND_QNNGPU = 1;
+    public static final int HEXAGON_BACKEND_QNNNPU = 2;
+    public static final int HEXAGON_BACKEND_CDSP   = 3;
+    public static final int HEXAGON_BACKEND_GGML   = 4;//"fake" HEXAGON backend for compare performance between HEXAGON backend and ggml backend
 
-         GGML_OP_MUL_MAT,
-         GGML_OP_MUL_MAT_ID,
-         GGML_OP_OUT_PROD,
-
-         GGML_OP_SCALE,
-         GGML_OP_SET,
-         GGML_OP_CPY,
-         GGML_OP_CONT,
-         GGML_OP_RESHAPE,
-         GGML_OP_VIEW,
-         GGML_OP_PERMUTE,
-         GGML_OP_TRANSPOSE,
-         GGML_OP_GET_ROWS,
-         GGML_OP_GET_ROWS_BACK,
-         GGML_OP_DIAG,
-         GGML_OP_DIAG_MASK_INF,
-         GGML_OP_DIAG_MASK_ZERO,
-         GGML_OP_SOFT_MAX,
-         GGML_OP_SOFT_MAX_BACK,
-         GGML_OP_ROPE,
-         GGML_OP_ROPE_BACK,
-         GGML_OP_CLAMP,
-         GGML_OP_CONV_TRANSPOSE_1D,
-         GGML_OP_IM2COL,
-         GGML_OP_IM2COL_BACK,
-         GGML_OP_CONV_TRANSPOSE_2D,
-         GGML_OP_POOL_1D,
-         GGML_OP_POOL_2D,
-         GGML_OP_POOL_2D_BACK,
-         GGML_OP_UPSCALE, // nearest interpolate
-         GGML_OP_PAD,
-         GGML_OP_PAD_REFLECT_1D,
-         GGML_OP_ARANGE,
-         GGML_OP_TIMESTEP_EMBEDDING,
-         GGML_OP_ARGSORT,
-         GGML_OP_LEAKY_RELU,
-
-         GGML_OP_FLASH_ATTN_EXT,
-         GGML_OP_FLASH_ATTN_BACK,
-         GGML_OP_SSM_CONV,
-         GGML_OP_SSM_SCAN,
-         GGML_OP_WIN_PART,
-         GGML_OP_WIN_UNPART,
-         GGML_OP_GET_REL_POS,
-         GGML_OP_ADD_REL_POS,
-         GGML_OP_RWKV_WKV6,
-         GGML_OP_GATED_LINEAR_ATTN,
-
-         GGML_OP_UNARY,
-
-         GGML_OP_MAP_UNARY,
-         GGML_OP_MAP_BINARY,
-
-         GGML_OP_MAP_CUSTOM1_F32,
-         GGML_OP_MAP_CUSTOM2_F32,
-         GGML_OP_MAP_CUSTOM3_F32,
-
-         GGML_OP_MAP_CUSTOM1,
-         GGML_OP_MAP_CUSTOM2,
-         GGML_OP_MAP_CUSTOM3,
-
-         GGML_OP_CROSS_ENTROPY_LOSS,
-         GGML_OP_CROSS_ENTROPY_LOSS_BACK,
-         GGML_OP_OPT_STEP_ADAMW,
-
-         GGML_OP_COUNT,
-    };
+    //keep sync with ggml-hexagon.cpp
+    public static final int HWACCEL_QNN = 0;
+    public static final int HWACCEL_QNN_SINGLEGRAPH = 1;
+    public static final int HWACCEL_CDSP = 2;
 
 
-     public static native int  asr_init(String strModelPath, int nThreadCounts, int nASRMode, int nBackendType);
+    public static native int asr_init(String strModelPath, int nThreadCounts, int nASRMode, int nBackendType);
 
     public static native void asr_finalize();
 
@@ -113,36 +27,39 @@ public class ggmljava {
 
     public static native void asr_stop();
 
-    public static native int  asr_reset(String strModelPath, int nThreadCounts, int nASRMode, int nBackendType);
+    public static native int asr_reset(String strModelPath, int nThreadCounts, int nASRMode, int nBackendType);
 
     public static native String asr_get_systeminfo();
 
     public static native int get_cpu_core_counts();
 
-
-     /**
-      * @param bExitBenchmark  0: reset internal status  1: exit/abort time-consuming bench task(such as LLM inference)
-      */
+    /**
+     * @param bExitBenchmark 0: reset internal status  1: exit/abort time-consuming bench task(such as LLM inference)
+     */
     public static native void ggml_set_benchmark_status(int bExitBenchmark);
 
     /**
-     * @param modelPath     /sdcard/kantv/ggml-xxxxxx.bin or  /sdcard/kantv/xxxxxx.gguf or qualcomm's prebuilt dedicated model.so or ""
-     * @param userData      ASR: /sdcard/kantv/jfk.wav / LLM: user input / TEXT2IMAGE: user input / MNIST: image path / TTS: user input
-     * @param nBenchType    0: memcpy 1: mulmat 2: ASR(whisper.cpp) 3: LLM(llama.cpp) 6: MNIST
+     * @param modelPath     /sdcard/kantv/ggml-xxxxxx.bin or  /sdcard/xxxxxx.gguf
+     * @param userData      ASR: /sdcard/kantv/jfk.wav or LLM: user input from UI
+     * @param nBenchType    0: memcpy 1: mulmat 2: ASR(whisper.cpp) 3: LLM(llama.cpp)
      * @param nThreadCounts 1 - 8
-     * @param nBackendType  0: ggml 1: NPU
-     * @param nOpType       type of matrix manipulate / GGML OP / type of various complex/complicated computation graph
+     * @param nBackendType  0: HEXAGON_BACKEND_QNNCPU 1: HEXAGON_BACKEND_QNNGPU 2: HEXAGON_BACKEND_QNNNPU, 3: HEXAGON_BACKEND_CDSP 4: ggml
+     * @param nHWAccelType  0: HWACCEL_QNN 1: HWACCEL_QNN_SINGLEGRAPH 2: HWACCEL_CDSP
      * @return
      */
-    public static native String ggml_bench(String modelPath, String userData, int nBenchType, int nThreadCounts, int nBackendType, int nOpType);
-
-    //05-25-2024, add for MiniCPM-V(A GPT-4V Level Multimodal LLM, https://github.com/OpenBMB/MiniCPM-V) or other GPT-4o style Multimodal LLM)
-    //"m" for "multimodal"
-    public static native String ggml_bench_m(String modelPath, String imgPath, String userData, int nBenchType, int nThreadCounts, int nBackendType);
-
+    public static native String ggml_bench(String modelPath, String userData, int nBenchType, int nThreadCounts, int nBackendType, int nHWAccelType);
 
     public static native String llm_get_systeminfo();
 
 
-    public static native String llm_inference(String modelPath, String prompt, int nBenchType, int nThreadCounts, int nBackendType);
+    /**
+     * @param modelPath     /sdcard/kantv/ggml-xxxxxx.bin or  /sdcard/xxxxxx.gguf
+     * @param prompt        user input from UI
+     * @param nLLMType      not used currently
+     * @param nThreadCounts 1 - 8
+     * @param nBackendType  0: HEXAGON_BACKEND_QNNCPU 1: HEXAGON_BACKEND_QNNGPU 2: HEXAGON_BACKEND_QNNNPU, 3: HEXAGON_BACKEND_CDSP 4: ggml
+     * @param nHWAccelType  0: HWACCEL_QNN 1: HWACCEL_QNN_SINGLEGRAPH 2: HWACCEL_CDSP
+     * @return
+     */
+    public static native String llm_inference(String modelPath, String prompt, int nLLMType, int nThreadCounts, int nBackendType, int nHWAccelType);
 }

android/kantvplayer-lib/src/main/java/kantvai/media/player/KANTVAssetLoader.java
@@ -52,11 +52,12 @@
 
              if (destFile.exists()) {
                  if (destFilePath.contains("ggml-hexagon.cfg")) {
+                     //ensure using updated ggml-hexagon.cfg
                      destFile.delete();
                  }
              }
 
-             if (destFile.exists()) {
+             if (destFile.exists() && destFile.isFile()) {
                  // 03-30-2024 17:09:35.152 24446 24797 D KANTV   : [LogUtils.cpp, logStdoutCallback, 48]:
                  // 0.3ms [ ERROR ] Unable to load backend. pal::dynamicloading::dlError():
                  // dlopen failed: file offset for the library "/data/data/com.kantvai.kantv/libQnnCpu.so" >= file size: 0 >= 0
@@ -69,18 +70,13 @@
                  // is not accessible for the namespace "clns-4"
 
                  KANTVLog.j(TAG, "dst file already exist");
-                 if (destFile.getAbsolutePath().contains("libQnnCpu")) {
-                     KANTVLog.j(TAG, "remove libQnnCpu.so");
-                     //destFile.delete();
+                 if (0 == destFile.length()) {
+                     destFile.delete();
+                     destFile.createNewFile();
+                 } else {
+                     return;
                  }
-                 if (destFile.getAbsolutePath().contains("libInception_v3")) {
-                     KANTVLog.j(TAG, "remove libInception_v3.so");
-                     //destFile.delete();
-                 }
-                 return;
-             }
-
-             if (!destFile.exists()) {
+             } else {
                  destFile.createNewFile();
              }
 
@@ -111,6 +107,7 @@
                  return;
              } else {
                  KANTVLog.j(TAG, "dir: " + destDirPath + " not exist");
+                 destFile.mkdirs();
              }
 
              String fileNames[] = context.getAssets().list(srcDirPath);

commit b08b2df2e7958b99507debd24eafb9a50b596567

AI-subtitle: fix issue of AI-subtitle which introduced in https://github.com/kantv-ai/kantv/pull/281

android/kantvplayer/src/main/java/com/kantvai/kantvplayer/player/ffplayer/FFPlayerView.java
@@ -1574,12 +1574,21 @@ public class FFPlayerView extends FrameLayout implements PlayerViewListener {
         }
 
         if (KANTVUtils.getASRSubsystemInit()) {
+            int result = 0;
             if ((KANTVUtils.ASR_MODE_NORMAL == mSettings.getASRMode()) || (KANTVUtils.ASR_MODE_TRANSCRIPTION_RECORD == mSettings.getASRMode())) {
-                ggmljava.asr_reset(KANTVUtils.getDataPath() + "/models/" + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_NORMAL, ggmljava.HEXAGON_BACKEND_GGML);
+                result = ggmljava.asr_reset(KANTVUtils.getDataPath() + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_NORMAL, ggmljava.HEXAGON_BACKEND_GGML);
             } else {
-                ggmljava.asr_reset(KANTVUtils.getDataPath() + "/models/" + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_PRESURETEST, ggmljava.HEXAGON_BACKEND_GGML);
+                result = ggmljava.asr_reset(KANTVUtils.getDataPath() + ggmlModelFileName, mSettings.getASRThreadCounts(), KANTVUtils.ASR_MODE_PRESURETEST, ggmljava.HEXAGON_BACKEND_GGML);
             }
-            ggmljava.asr_start();
+            if (0 == result) {
+                ggmljava.asr_start();
+            } else {
+                KANTVLog.j(TAG, "failed to initialized ASR");
+                topBarView.updateTVASRVisibility(false);
+                KANTVUtils.setTVASR(false);
+                KANTVUtils.showMsgBox(mAttachActivity, "ASR subsystem not initialized, pls check why");
+                return;
+            }
         } else {
             topBarView.updateTVASRVisibility(false);
             KANTVUtils.setTVASR(false);

android/kantvplayer/src/main/java/com/kantvai/kantvplayer/ui/fragment/AIResearchFragment.java
@@ -559,7 +559,12 @@
              ggmlModelFileName = selectModelFilePath;
              KANTVLog.j(TAG, "model file:" + selectModelFilePath);
              if (isASRModel) { //avoid crash
-                 ggmljava.asr_reset(selectModelFilePath, ggmljava.get_cpu_core_counts() / 2, KANTVUtils.ASR_MODE_BECHMARK, backendIndex);
+                 int result = ggmljava.asr_reset(selectModelFilePath, ggmljava.get_cpu_core_counts() / 2, KANTVUtils.ASR_MODE_BECHMARK, backendIndex);
+                 if (0 != result) {
+                     KANTVLog.j(TAG, "failed to initialize ASR");
+                     KANTVUtils.showMsgBox(mActivity, "failed to initialize ASR");
+                     return;
+                 }
              }
 
              nLogCounts = 0;

core/ggml/jni/ggml-jni-impl.cpp
@@ -635,7 +635,7 @@ static const char * ggml_jni_transcribe_from_file(const char * sz_model_path, co
         wcp.gpu_device  = n_backend_type;
         wcp.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
-        if (n_backend_type != 1) {
+        if (n_backend_type != HEXAGON_BACKEND_GGML) {
             wcp.use_gpu = true;
         } else {
             wcp.use_gpu = false;
@@ -1457,7 +1457,7 @@ int whisper_asr_init(const char * sz_model_path, int n_threads, int n_asrmode, i
      c_params.gpu_device  = n_backend;
      c_params.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
-     if (n_backend != 1) {
+     if (n_backend != HEXAGON_BACKEND_GGML) {
          c_params.use_gpu = true;
      } else {
          c_params.use_gpu = false;
@@ -1643,7 +1643,7 @@ int whisper_asr_reset(const char * sz_model_path, int n_threads, int n_asrmode, 
         wcp.gpu_device  = n_backend;
         wcp.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
-        if (n_backend != 1) {
+        if (n_backend != HEXAGON_BACKEND_GGML) {
             wcp.use_gpu = true;
         } else {
             wcp.use_gpu = false;
@@ -1661,7 +1661,21 @@ int whisper_asr_reset(const char * sz_model_path, int n_threads, int n_asrmode, 
         wcp.gpu_device  = n_backend;
         wcp.use_gpu     = false;
 #ifdef GGML_USE_HEXAGON
-        if (n_backend != 1) {
+        if (n_backend != HEXAGON_BACKEND_GGML) {
+            wcp.use_gpu = true;
+        } else {
+            wcp.use_gpu = false;
+        }
+#endif
+        p_asr_ctx->p_context = whisper_init_from_file_with_params(sz_model_path, wcp);
+        p_asr_ctx->n_backend = n_backend;
+    } else if (nullptr == p_asr_ctx->p_context) {
+        LOGGD("re-init whispercpp instance");
+        struct whisper_context_params wcp = whisper_context_default_params();
+        wcp.gpu_device  = n_backend;
+        wcp.use_gpu     = false;
+#ifdef GGML_USE_HEXAGON
+        if (n_backend != HEXAGON_BACKEND_GGML) {
             wcp.use_gpu = true;
         } else {
             wcp.use_gpu = false;
@@ -1674,6 +1688,7 @@ int whisper_asr_reset(const char * sz_model_path, int n_threads, int n_asrmode, 
     }
 
     if (nullptr == p_asr_ctx->p_context) {
+        LOGGW("it should not happen and ASR feature would be not available, pls check why?\n");
         result = 3;
     } else {
         p_asr_ctx->n_threads            = n_threads;

core/ggml/whispercpp/whisper.cpp
@@ -1282,11 +1282,13 @@ static ggml_backend_t whisper_backend_init_gpu(const whisper_context_params & pa
 static std::vector<ggml_backend_t> whisper_backend_init(const whisper_context_params & params) {
     std::vector<ggml_backend_t> result;
 
+#if 0 //forcefully disable ggml-hexagon backend because there is unknown issue with ASR inference through cDSP, refer to:https://github.com/kantv-ai/kantv/pull/281
     ggml_backend_t backend_gpu = whisper_backend_init_gpu(params);
 
     if (backend_gpu) {
         result.push_back(backend_gpu);
     }
+
 
     // ACCEL backends
     for (size_t i = 0; i < ggml_backend_dev_count(); ++i) {
@@ -1305,7 +1307,7 @@ static std::vector<ggml_backend_t> whisper_backend_init(const whisper_context_pa
             result.push_back(backend);
         }
     }
-
+#endif
     GGML_UNUSED(params);
 
     result.push_back(ggml_backend_cpu_init());